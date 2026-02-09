b1e2f5b451f7cd9591bfe534f3d8a628313c3325
WindSnowLi <wzmmdsnn@163.com>
2026-02-09 17:34:48 +0800

    op ---

--- Toast/Toast.h
+++ Toast/Toast.h
@@ -2,8 +2,8 @@
 
 #include "toast_global.h"
 
-#include <QReadWriteLock>
 #include <QMessageBox>
+#include <QReadWriteLock>
 #include <QWidget>
 
 #ifndef TOAST_TIP
@@ -11,12 +11,24 @@
 #endif // !TOAST_TIP
 
 #ifndef TOAST_TIP_P
-#define TOAST_TIP_P(text, type,parent) Toast::ShowTip(text, type, parent)
+#define TOAST_TIP_P(text, type, parent) Toast::ShowTip(text, type, parent)
 #endif // !TOAST_TIP_P
 
 #ifndef TOAST_TIP_T
 #define TOAST_TIP_T(text, type) Toast::ShowTip(text, type)
 #endif // !TOAST_TIP_T
+
+#ifndef TOAST_INFO
+#define TOAST_INFO(text) Toast::ShowTip(text, QMessageBox::Information)
+#endif // !TOAST_INFO
+
+#ifndef TOAST_WARNING
+#define TOAST_WARNING(text) Toast::ShowTip(text, QMessageBox::Warning)
+#endif // !TOAST_WARNING
+
+#ifndef TOAST_ERROR
+#define TOAST_ERROR(text) Toast::ShowTip(text, QMessageBox::Critical)
+#endif // !TOAST_ERROR
 
 class TOAST_EXPORT Toast : public QWidget {
     Q_OBJECT
@@ -34,5 +46,5 @@ public:
      * @brief 静态调用
      * @param text 文字内容
      */
-    static void ShowTip(const QString& text, QMessageBox::Icon type= QMessageBox::Information, QWidget* parent = nullptr);
+    static void ShowTip(const QString& text, QMessageBox::Icon type = QMessageBox::Information, QWidget* parent = nullptr);
 };
